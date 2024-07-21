04ef11c434a09f22004028423c553d334e33b8be
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2024-07-22 01:41:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-colour';
 
 function App() {
   return (
     <div className="App">
-      <Accordian/>
+      {/*<Accordian/>*/}
+
+      <RandomColor/>
     </div>
   );
 }
